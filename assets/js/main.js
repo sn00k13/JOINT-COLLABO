7c88a1708fc042439d21ebc5a933706be408048b
sn00k13 <gocitek@gmail.com>
2024-07-26 04:00:19 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,19 @@
+//This controls the slide show display
+
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-    slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
 
 // const hamburger = document.getElementById('hamburger');
 // const hamburgerMenu = document.getElementById('hamburgerMenu');
